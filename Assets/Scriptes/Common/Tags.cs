3afb06983b0cec35ee855732069cb99fb13ca1a6
Solomatov Nikolai <nikosol94@gmail.com>
2021-11-07 20:00:52 +0700

    Добавил звукового разнообразия

--- Assets/Scriptes/Common/Tags.cs
+++ Assets/Scriptes/Common/Tags.cs
@@ -5,6 +5,9 @@
         public static readonly string PLATE_ACCEPTOR = "PlateAcceptor";
         public static readonly string FLOOR = "Floor";
         public static readonly string SANDWICH = "Sandwich";
+        public static readonly string GARBAGE = "Garbage";
+        public static readonly string GARBAGE_METAL = "Garbage.metal";
+        public static readonly string GARBAGE_BRICK = "Garbage.Brick";
 
         public static bool IfPlateOrFloor(string tag) {
             return tag == PLATE || tag == FLOOR;
---
+++ Assets/Scriptes/Sounds/AudioActionListener.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Scriptes.UI.FinishScreen;
+using UnityEngine;
+
+namespace Assets.Scriptes.Sounds {
+    public class AudioActionListener : MonoBehaviour {
+
+        [SerializeField] private AudioSource[] BoohSources;
+        [SerializeField] private AudioSource[] BoomSources;
+        [SerializeField] private AudioSource[] ZingSources;
+        [SerializeField] private AudioSource[] FinishGame;
+
+        private void Start() {
+            AudioActionInvoker.OnBooh+= PlayBooh;
+            FinishScreenBahaviour.OnFinishGame += PlayFinishGame;
+            AudioActionInvoker.OnBoom += PlayBoom;
+            AudioActionInvoker.OnZing += PlayZing;
+        }
+
+        private void PlayZing() {
+            var voice = ZingSources[Random.Range(0, ZingSources.Length)];
+            voice.pitch = Random.Range(0.9f, 1.1f);
+            voice.Play();
+        }
+
+        private void PlayBoom() {
+            var voice = BoomSources[Random.Range(0, BoomSources.Length)];
+            voice.pitch = Random.Range(0.9f, 1.1f);
+            voice.Play();
+        }
+
+        private void PlayBooh() {
+            var voice = BoohSources[Random.Range(0, BoohSources.Length)];
+            voice.pitch = Random.Range(0.9f, 1.1f);
+            voice.Play();
+        }
+        private void PlayFinishGame() {
+            var voice = FinishGame[Random.Range(0, FinishGame.Length)];
+            voice.pitch = Random.Range(0.9f, 1.1f);
+            voice.Play();
+        }
+
+    }
+}

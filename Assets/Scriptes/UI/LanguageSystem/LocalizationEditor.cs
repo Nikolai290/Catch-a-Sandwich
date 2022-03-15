e11e273b9d36e204bcde5d16e9e7979918e254dc
Solomatov Nikolai <nikosol94@gmail.com>
2022-03-15 22:45:50 +0700

    feat: Localization
    added localization changer
    added languages: Russian, English,Spanish

---
+++ Assets/Scriptes/UI/LanguageSystem/LocalizationEditor.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scriptes.UI.LanguageSystem {
+    [CustomEditor(typeof(TextSubstitutor))]
+	public class LocalizationEditor : Editor {
+
+		public override void OnInspectorGUI() {
+			DrawDefaultInspector();
+			TextSubstitutor lcz = (TextSubstitutor)target;
+			if (GUILayout.Button("Build Default Locale")) {
+				lcz.BuildDefaultLocale();
+			}
+		}
+	}
+}
+#endif
